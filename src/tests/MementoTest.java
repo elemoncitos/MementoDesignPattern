87ce2d782a8f2cd45484710a42288c2a81f74a23
elemoncitos <elemosgue@alum.us.es>
2021-02-09 15:07:49 +0100

    Creacion de la clase de Tests aunque no implementadas

---
+++ src/tests/MementoTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MementoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
